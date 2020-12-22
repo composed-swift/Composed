87ba742b789445c7d59752a8564c3167c91170b6
Joseph Duffy <commits@josephduffy.co.uk>
2020-12-22 13:59:21 +0000

    Update Quick and Nimble

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
             targets: ["ComposedUI"]),
     ],
     dependencies: [
-        .package(name: "Quick", url: "https://github.com/quick/quick", from: "2.0.0"),
-        .package(name: "Nimble", url: "https://github.com/quick/nimble", from: "8.0.0"),
+        .package(name: "Quick", url: "https://github.com/Quick/Quick.git", from: "3.0.0"),
+        .package(name: "Nimble", url: "https://github.com/Quick/Nimble.git", from: "9.0.0"),
     ],
     targets: [
         .target(name: "Composed"),
