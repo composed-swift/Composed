2c235434843d3b7bd0b64662fe5c26fc46656811
Joseph Duffy <commits@josephduffy.co.uk>
2021-04-14 15:37:22 +0100

    Add extra `UICollectionView` test

--- Tests/ComposedUITests/UICollectionViewTests.swift
+++ Tests/ComposedUITests/UICollectionViewTests.swift
@@ -42,4 +42,48 @@ final class UICollectionViewTests: XCTestCase {
             viewController.collectionView.reloadData()
         }
     }
+
+    func testUpdatingBeforeBatchUpdatesWhenViewNeedsLayout() throws {
+        try XCTSkipIf(true, "This test will purposefully fail; it is validating that `performBatchUpdates` will crash when changes are applied before being called when the layout has not been updated.")
+
+        final class CollectionViewController: UICollectionViewController {
+            var data: [[String]] = []
+
+            override func viewDidLoad() {
+                super.viewDidLoad()
+
+                collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "UICollectionViewCell")
+            }
+
+            override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+                collectionView.dequeueReusableCell(withReuseIdentifier: "UICollectionViewCell", for: indexPath)
+            }
+
+            override func numberOfSections(in collectionView: UICollectionView) -> Int {
+                data.count
+            }
+
+            override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+                data[section].count
+            }
+        }
+
+        let viewController = CollectionViewController(collectionViewLayout: UICollectionViewFlowLayout())
+        viewController.data = [
+            [
+                "0, 0",
+                "0, 1",
+            ]
+        ]
+        viewController.loadViewIfNeeded()
+
+        // Uncommenting this line allows the tests to pass because it ensures the layout is
+        // updated before `performBatchUpdates` has been called
+//        viewController.collectionView.layoutIfNeeded()
+        viewController.data.append(["1, 0"])
+
+        viewController.collectionView.performBatchUpdates {
+            viewController.collectionView.insertSections([1])
+        }
+    }
 }
