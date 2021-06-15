07548ff63b143707b8e2c0b371b3263121f6abf3
Joseph Duffy <commits@josephduffy.co.uk>
2021-06-15 13:34:38 +0100

    Add test validating reloads handled before deletes

--- Tests/ComposedUITests/UICollectionViewTests.swift
+++ Tests/ComposedUITests/UICollectionViewTests.swift
@@ -86,4 +86,50 @@ final class UICollectionViewTests: XCTestCase {
             viewController.collectionView.insertSections([1])
         }
     }
+
+    /// A test to validate that section reloads are handled before section deletes.
+    func testReloadsAreHandledBeforeRemoves() {
+        final class CollectionViewController: UICollectionViewController {
+            var data: [[String]] = []
+
+            var requestedIndexPaths: [IndexPath] = []
+
+            override func viewDidLoad() {
+                super.viewDidLoad()
+
+                collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "UICollectionViewCell")
+            }
+
+            override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+                collectionView.dequeueReusableCell(withReuseIdentifier: "UICollectionViewCell", for: indexPath)
+            }
+
+            override func numberOfSections(in collectionView: UICollectionView) -> Int {
+                data.count
+            }
+
+            override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+                data[section].count
+            }
+        }
+
+        let viewController = CollectionViewController(collectionViewLayout: UICollectionViewFlowLayout())
+        viewController.data = [
+            ["0, 0"],
+            ["1, 0"],
+            ["2, 0"],
+        ]
+        viewController.loadViewIfNeeded()
+        viewController.collectionView.reloadData()
+        viewController.requestedIndexPaths = []
+
+        viewController.collectionView.performBatchUpdates({
+            viewController.data.remove(at: 1)
+            viewController.data[1] = ["2, 0 (new)"]
+            viewController.collectionView.deleteSections([1])
+            viewController.collectionView.reloadSections([2])
+        }, completion: { _ in
+            XCTAssertEqual(viewController.requestedIndexPaths, [IndexPath(item: 0, section: 1)])
+        })
+    }
 }
